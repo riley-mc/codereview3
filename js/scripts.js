d20edee71ff719c579682495b9d74980182f2869
Riley McAdoo <riley.mcadoo@gmail.com>
2020-10-30 14:28:35 -0700

    More messing with JS. Almost got it I think.

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,23 @@
 // Business Logic
-var userInputResults = function(number) {
-  var result = [];
+function roboSwap(num, name) {
+  const numArray = [];
+  for (let index = 0; index <= num; i++)  {
+    let check = index.toString();
 
-  for (index = 0; index <= number; index++) {
-    result.push(" " + index);
-    var string = index.toString();
+    if (check.includes(3))  {
+      numArray.push("Won't you be my neighbor" + "?")
+    } else if (check.includes(2)){
+      numArray.push("Boop")
+    } else if (check.includes(1)){
+      numArray.push("Beep")
+    } else {
+      numArray.push(index)
+    }
 
-    if (string.includes("3")){
-    result.splice(index, 1, "Will you be my neighbor?")
-  } else if (string.includes("2")){
-    result.splice(index, 1, "Boop")
-  } else if (string.includes("1")){
-    result.splice(index, 1, "Beep")
-  }
-}
-  return results;
-}
-    
+      }
+return numArray
+    }
+  
 
 
 
@@ -30,7 +31,6 @@ $(document).ready(function()  {
     $("#number").val (" "); // Might not need.
     $("#container").text(results);
     $("results").show();
-    $("output").append(results);
 
   });
 });
@@ -40,3 +40,29 @@ $(document).ready(function()  {
 
 // let num = $("input#num").val();
 // let result = roboSwap(num);
+
+
+
+
+
+
+
+// var userInputResults = function(number) {
+//   var result = [];
+//   //var string = index.toString();
+//   for (index = 0; index <= number; index++) {
+//     result.push(" " + index);
+//     var string = index.toString();
+
+//     if (string.includes("3")){
+//     result.splice(index, 1, "Will you be my neighbor?")
+//   } else if (string.includes("2")){
+//     result.splice(index, 1, "Boop")
+//   } else if (string.includes("1")){
+//     result.splice(index, 1, "Beep")
+//   }
+// }
+//   return result;
+// }
+    
+
