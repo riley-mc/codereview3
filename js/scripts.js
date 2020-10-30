0f3497ddb6bf1ec10815c446ac469ecbdde31b18
Riley McAdoo <riley.mcadoo@gmail.com>
2020-10-30 09:54:13 -0700

    Add start of JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,8 @@
 // Business Logic
+function roboSwap(num)  {
+  const numAray = [];
 
+}
 
 
 
@@ -16,7 +19,17 @@
 
 //User Interface Logic
 
+$(document).ready(function()  {
+  $("form#text").submit(function() {
+    event.preventDefault();
 
+    let num = $("input#num").val();
+    let result = roboSwap(num);
+
+    $("output").append(results);
+
+  });
+});
 
 
 
